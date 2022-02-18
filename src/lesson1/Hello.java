0c68859c3a14393dedf32023be8aa47825f9b76c
Freddie Kyalo <fredngara@gmail.com>
2022-02-18 08:13:13 +0300

    first commit

---
+++ src/lesson1/Hello.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+public class Hello extends student {
+	public final int number;
+	
+	Hello(){
+		number = 10;
+	}
+
+}
---
+++ src/lesson1/MyClass.java
@@ -0,0 +1,16 @@
+package lesson1;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hello hel = new Hello();
+		//hel.number = 20;
+		
+		
+		student stu = new student();
+		stu.setName("Mark");
+
+	}
+
+}
